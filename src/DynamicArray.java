d9188df29c54e52ff1906beceb721f63a2979814
gglcrash <ggl.crash@gmail.com>
2016-11-02 00:28:08 +0300

    init

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 /**
  * Created by gglcrash on 01.11.2016.
  */
@@ -11,9 +13,26 @@ public class DynamicArray {
 
     public DynamicArray(int count){
         array = new int[count];
+
     }
 
     public DynamicArray(){
-        array = new int[DEFAULT_SIZE];
+        //array = new int[DEFAULT_SIZE];
+    }
+
+    public void Add(int value){
+
+    }
+
+    public void Insert(int position, int value){
+
+    }
+
+    public void Delete(int position){
+
+    }
+
+    public void Sort(Comparator<Integer> comparator){
+
     }
 }
--- src/MyComparator.java
+++ src/MyComparator.java
@@ -3,6 +3,11 @@ import java.util.Comparator;
 /**
  * Created by gglcrash on 01.11.2016.
  */
-public class MyComparator {
+public class MyComparator implements Comparator<Integer> {
 
+    @Override
+    public int compare(Integer o1, Integer o2) {
+
+        return 0;
+    }
 }
